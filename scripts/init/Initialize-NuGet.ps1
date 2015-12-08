b3ce49c8f364feb60e3a9b41127a5e9d245746a2
Jah Emanuel <jahquin@microsoft.com>
2015-12-07 16:24:52 -0800

    Updating to latest bootstrapper scripts

--- scripts/init/Initialize-NuGet.ps1
+++ scripts/init/Initialize-NuGet.ps1
@@ -10,23 +10,17 @@ $toolsDir = Join-Path -Resolve $repoRoot ".tools"
 
 # Ensure nuget.exe is up-to-date
 $nugetDownloadName = "nuget.exe"
-$nugetDest = . "$PSScriptRoot\Initialize-DownloadLatest.ps1" -OutDir $toolsDir -DownloadUrl "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -DownloadName $nugetDownloadName -Unzip $false
+. "$PSScriptRoot\Initialize-DownloadLatest.ps1" -OutDir $toolsDir -DownloadUrl "https://dist.nuget.org/win-x86-commandline/latest/nuget.exe" -DownloadName $nugetDownloadName -Unzip $false
 
-# Add the tools dir to the path
+# Ensure VSS.NuGet.AuthHelper is up-to-date
+$credProviderDownloadFeed = "https://nuget.org/api/v2/"
+$credProviderPackageName = "Microsoft.VisualStudio.Services.NuGet.CredentialProvider"
+$credProviderExeName = "CredentialProvider.VSS.exe"
+
+. "$PSScriptRoot\Initialize-InstallFromNuget.ps1" -OutDir $toolsDir -DownloadFeed $credProviderDownloadFeed -PackageName $credProviderPackageName -targetFileName $credProviderExeName
+
+# Add the tools dir to the path which directly contains NuGet.exe and VSS.NuGet.AuthHelper.exe
 if (!($env:Path -like "*$toolsDir;*"))
 {
     $env:Path = "$toolsDir;" + $env:Path
-}
-
-# Ensure VSS.NuGet.AuthHelper is up-to-date
-$authHelperDownloadFeed = "https://nuget.org/api/v2/"
-$authHelperPackageName = "Microsoft.VisualStudio.Services.NuGet.AuthHelper"
-$authHelperExeName = "VSS.NuGet.AuthHelper.exe"
-
-$authHelperDest = . "$PSScriptRoot\Initialize-InstallFromNuget.ps1" -OutDir $toolsDir -DownloadFeed $authHelperDownloadFeed -PackageName $authHelperPackageName -targetFileName $authHelperExeName
-
-# Add VSS.NuGet.AuthHelper to the path
-if (!($env:Path -like "*$authHelperDest;*"))
-{
-    $env:Path = "$authHelperDest;" + $env:Path
 }
